87755bdcfecc04941533a27123cf03a92e8fcbb8
Jorge Martin <jorge.martin@pulselive.com>
2023-06-30 12:13:49 +0200

    Exercise 6.9 Update repository with CRUD methods

--- src/main/kotlin/com/jmartinal/repositories/NotesRepository.kt
+++ src/main/kotlin/com/jmartinal/repositories/NotesRepository.kt
@@ -1,16 +1,45 @@
 package com.jmartinal.repositories
 
 import com.jmartinal.models.Note
-import com.jmartinal.models.Note.Type.AUDIO
-import com.jmartinal.models.Note.Type.TEXT
 
 object NotesRepository {
 
-    fun getAll(size: Int = 10): List<Note> = (1..size).map { position ->
-        Note(
-            title = "Title $position",
-            description = "Description $position",
-            type = if (position % 3 == 0) AUDIO else TEXT
-        )
+    private val notes = mutableListOf<Note>()
+    private var currentId: Long = 1L
+
+    // CREATE
+    fun save(note: Note): Note {
+        val noteToSave = note.copy(id = currentId++)
+        notes.add(noteToSave)
+        return noteToSave
+    }
+
+    // READ
+    fun getAll(): List<Note> = notes
+
+    fun getById(id: Long): Note? = notes.find { it.id == id }
+
+    // UPDATE
+    fun update(note: Note): Boolean {
+        val index = notes.indexOfFirst { it.id == note.id }
+        return when(index >= 0) {
+            true -> {
+                notes[index] = note
+                true
+            }
+            false -> false
+        }
+    }
+
+    // DELETE
+    fun delete(id: Long): Boolean {
+        val index = notes.indexOfFirst { it.id == id }
+        return when(index >= 0) {
+            true -> {
+                notes.removeAt(index)
+                true
+            }
+            false -> false
+        }
     }
 }
