46ec14e04d15cdbe2dc235b9177eef4004c8ca0a
Jorge Martin <jorge.martin@pulselive.com>
2023-07-01 20:00:49 +0200

    Exercise 6.13 Persistent database creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,24 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val sqldelight_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.serialization") version "1.8.22"
     id("io.ktor.plugin") version "2.3.2"
+    id("com.squareup.sqldelight") version "1.5.5"
 }
 
 group = "com.jmartinal"
 version = "0.0.1"
+
+sqldelight {
+    database("MyNotesDatabase") {
+        packageName = "com.jmartinal.mynotes.database"
+    }
+}
+
 application {
     mainClass.set("com.jmartinal.ApplicationKt")
 
@@ -29,6 +38,8 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("com.squareup.sqldelight:sqlite-driver:$sqldelight_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
