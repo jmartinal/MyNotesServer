b4afd5b180caca777c798864f5db8f11891489cd
Jorge Martin <jorge.martin@pulselive.com>
2023-12-04 12:17:55 +0100

    Exercise 8.4 Added CORS plugin to allow client requests from web pages to other web pages

--- src/main/kotlin/com/jmartinal/Application.kt
+++ src/main/kotlin/com/jmartinal/Application.kt
@@ -1,17 +1,23 @@
 package com.jmartinal
 
+import com.jmartinal.plugins.routing.configureRouting
+import com.jmartinal.plugins.serialization.configureSerialization
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.jmartinal.plugins.routing.configureRouting
-import com.jmartinal.plugins.serialization.configureSerialization
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
-fun Application.module() {
-    configureRouting()
-    configureSerialization()
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+        install(CORS) {
+            anyHost() // In a complex app we should only allow known hosts, not every host
+            allowNonSimpleContentTypes = true // To be able to send complex content via POST requests
+            allowHeader(HttpHeaders.AccessControlAllowOrigin) // The header that the client uses to send CORS information
+            allowMethod(HttpMethod.Put)
+            allowMethod(HttpMethod.Delete)
+        }
+    }.start(wait = true)
 }
